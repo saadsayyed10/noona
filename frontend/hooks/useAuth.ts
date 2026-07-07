a5a0ecab2a9a6f9f4aef1d48e289697ae873c4fd
Saad Sayyed <saadsyed950@gmail.com>
2026-07-07 16:51:38 +0530

    Auth hook

---
+++ frontend/hooks/useAuth.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+interface AuthState {
+  token: string | null;
+  user: any | null;
+  setAuth: (token: string, user: any) => Promise<void>;
+  logout: () => Promise<void>;
+  hydrate: () => Promise<void>;
+}
+
+export const useAuth = create<AuthState>((set) => ({
+  token: null,
+  user: null,
+
+  setAuth: async (token, user) => {
+    await AsyncStorage.setItem("token", token);
+    await AsyncStorage.setItem("user", user);
+    set({ token, user });
+  },
+
+  logout: async () => {
+    await AsyncStorage.removeItem("token");
+    await AsyncStorage.removeItem("user");
+    set({ token: null, user: null });
+  },
+
+  hydrate: async () => {
+    const token = await AsyncStorage.getItem("token");
+    const user = await AsyncStorage.getItem("user");
+
+    set({ token: token, user: user ? JSON.parse(user) : null });
+  },
+}));
