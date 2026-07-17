51177f71e285e668169e6d5acd45670deb898873
Saad Sayyed <saadsyed950@gmail.com>
2026-07-17 16:40:13 +0530

    Send invite API (fixed)

--- backend/src/api/routes/invitation.route.ts
+++ backend/src/api/routes/invitation.route.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import * as controllers from "../controllers/invitation.controller";
+import { protectAuth } from "../../middleware/authMiddleware";
 
 const invitationRouter = Router();
 
-invitationRouter.post("/:guestId", controllers.inviteFriendController);
+invitationRouter.post(
+  "/:guestId",
+  protectAuth,
+  controllers.inviteFriendController,
+);
 
 export default invitationRouter;
