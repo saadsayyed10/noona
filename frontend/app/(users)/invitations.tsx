56a8de1ea9a31fcb9c9722b1f3455dcbb9960db2
Saad Sayyed <saadsyed950@gmail.com>
2026-07-21 19:09:31 +0530

    Starting to work on invitations

---
+++ frontend/app/(users)/invitations.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Invitations = () => {
+  return (
+    <View>
+      <Text>Invitations</Text>
+    </View>
+  );
+};
+
+export default Invitations;
