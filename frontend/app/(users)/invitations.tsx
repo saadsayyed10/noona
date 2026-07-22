5c2dc9661cc47f7abd259672935ee9a06cfe902a
Saad Sayyed <saadsyed950@gmail.com>
2026-07-22 16:53:01 +0530

    Invitation/Friends module complete

--- frontend/app/(users)/invitations.tsx
+++ frontend/app/(users)/invitations.tsx
@@ -123,12 +123,6 @@ const Invitations = () => {
             onRefresh={onRefresh}
             tintColor={"#9d73d8"}
             colors={["#9d73d8"]}
-            style={{
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              width: "100%",
-            }}
           />
         }
       >
