c0637df6bdb279cc18b5f4a959e5ceee0b1de50c
Saad Sayyed <saadsyed950@gmail.com>
2026-08-27 00:22:49 +0530

    Controller: Register user account

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,52 @@
+// backend\src\controllers\auth.controller.ts
+
+// Import (Request and Response) type instances
+import { Request, Response } from "express";
+
+// Import all services in an object
+import * as authServices from "../services/auth.service";
+
+// Global variable to handle error messages
+let errorMessage;
+
+/*
+    Controller: To register user account
+    Method: POST
+    Endpoint: /api/auth/sign-up
+    Authorization: No    
+*/
+export const registerUserController = async (req: Request, res: Response) => {
+  const { name, email, username, password } = req.body;
+
+  const data = { name, email, username };
+  // Throw error if fields are left null
+  if (!data) {
+    errorMessage = "Please do not leave any fields empty";
+    console.log(errorMessage);
+    return res.status(400).json({ error: errorMessage });
+  }
+
+  // Password validation
+  if (!password || password.length < 8) {
+    errorMessage = "Password should be more than or equal to 8 characters";
+    console.log(errorMessage);
+    return res.status(400).json({ error: errorMessage });
+  }
+
+  try {
+    const { token, user } = await authServices.registerUserService(
+      name,
+      email,
+      username,
+      password,
+    );
+
+    res.status(201).json({
+      message: `Dear ${user.name} thanks for creating the account, welcome to Noona.`,
+      token,
+    });
+  } catch (error: any) {
+    console.log(error.message);
+    return res.status(500).json({ error: error.message });
+  }
+};
