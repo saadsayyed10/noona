7dbec78cf005b062ce00ce1ca236c037ea81e1ac
Saad Sayyed <saadsyed950@gmail.com>
2026-08-26 20:28:14 +0530

    Starting with authentication part

---
+++ backend/src/routes/index.route.ts
@@ -0,0 +1,7 @@
+// backend\src\routes\index.route.ts
+
+import { Router } from "express";
+
+const mainRouter = Router(); // Instance of main endpoint ("/api")
+
+export default mainRouter;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,28 @@
+// backend\src\server.ts
+
+// Import all libraries
 import express, { Request, Response } from "express";
 import cors from "cors";
+
+// Import custom files
 import { env } from "./config/env.config";
 import { connectToDB } from "./config/db.config";
+import mainRouter from "./routes/index.route";
 
-const app = express();
-const PORT = env.PORT;
+const app = express(); // REST server instance
+const PORT = env.PORT; // Custom PORT
 
+// Middlewares
 app.use(express.json());
 app.use(cors());
+app.use("/api", mainRouter);
 
+// Check health API endpoint
 app.get("/ping", (_req: Request, res: Response) => {
   res.json({ status: 200 });
 });
 
+// Function to start server asynchronously
 const startServer = async () => {
   await connectToDB();
 
@@ -21,4 +31,4 @@ const startServer = async () => {
   });
 };
 
-startServer();
+startServer(); // Run server
