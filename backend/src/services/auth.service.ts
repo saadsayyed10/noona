6024624d1c50776258f2b1ba969d83fdcd5cf9ca
Saad Sayyed <saadsyed950@gmail.com>
2026-08-27 00:08:54 +0530

    Service: Register user account

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,6 +1,12 @@
 // backend\src\services\auth.service.ts
 
+// Import libraries
+import bcrypt from "bcryptjs";
+
+// Import custom files
 import prisma from "../lib/prisma";
+import { generatePfp } from "../lib/pfp";
+import { generateToken } from "../lib/token";
 
 export const registerUserService = async (
   name: string,
@@ -18,4 +24,24 @@ export const registerUserService = async (
   if (existingUser) throw new Error("Your account already exist");
 
   // Generate profile picture for user
+  const profilePicture = generatePfp(name);
+
+  // Hash user password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Register user data
+  const user = await prisma.users.create({
+    data: {
+      name,
+      email,
+      username,
+      password: hashedPassword,
+      profilePicture,
+    },
+  });
+
+  // Generate token for the particular user
+  const token = generateToken(user.id!);
+
+  return { token, user }; // Return token and user data
 };
